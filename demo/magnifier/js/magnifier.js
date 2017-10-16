42b2ba82b3d28bda58874bb5914a5b979f467523
fredispig <1030796841@qq.com>
2017-10-16 23:26:13 +0800

    完成

--- demo/magnifier/js/magnifier.js
+++ demo/magnifier/js/magnifier.js
@@ -13,9 +13,11 @@ window.onload= function () {
 
 
   var oleft=document.getElementById("left");
-    var oright=document.getElementById("right");
+     var oright=document.getElementById("right");
     var ofloatDiv=document.getElementById("floatDiv");
-    oleft.onmouseover=function(){
+    var bimg=document.getElementById("bimg");
+    var simg=document.getElementById("simg");
+      oleft.onmouseover=function(){
         oright.style.display="block";
         ofloatDiv.style.display="block";
     }
@@ -24,8 +26,28 @@ window.onload= function () {
         ofloatDiv.style.display="none";
     }
     oleft.onmousemove=function(ev){
+
+        var ofloatDivLeft=ev.clientX-ofloatDiv.offsetWidth/2;
+        var ofloatDivTop=ev.clientY-ofloatDiv.offsetHeight/2;
+        if(ofloatDivLeft<0){ofloatDivLeft=0}
+        if(ofloatDivTop<0){ofloatDivTop=0}
+        if(ofloatDivLeft>oleft.offsetWidth-ofloatDiv.offsetWidth){
+            ofloatDivLeft=oleft.offsetWidth-ofloatDiv.offsetWidth;
+        }
+        if(ofloatDivTop>oleft.offsetHeight-ofloatDiv.offsetHeight){
+            ofloatDivTop=oleft.offsetHeight-ofloatDiv.offsetHeight;
+        }
+        ofloatDiv.style.left=ofloatDivLeft+'px';
+        ofloatDiv.style.top=ofloatDivTop+'px';
+        /*设置大图的宽高*/
+        var biliw=oleft.offsetWidth/ofloatDiv.offsetWidth;
+        var bilih=oleft.offsetHeight/ofloatDiv.offsetHeight;
+        bimg.style.width=biliw*oright.offsetWidth+"px";
+        bimg.style.height=bilih*oright.offsetHeight+"px";
+
+        bimg.style.left=oright.style.left-ofloatDivLeft*biliw+"px";
+        bimg.style.top=oright.style.top-ofloatDivTop*bilih+"px";
+
         console.log(ev);
-        ev.clientX-;
-        ev.clientY;
     }
 }
